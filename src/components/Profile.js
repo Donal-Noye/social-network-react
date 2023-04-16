3003f92c4f3b487e848d557a9c891eb66f42a34e
Donal Noye <leekeiz49@gmail.com>
2023-04-16 11:52:41 +0300

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,6 @@ export default function Profile() {
           <img className="w-full h-full object-cover" src="https://placehold.co/70" alt=""/>
         </div>
         <h2 className="text-2xl">Donal Noye</h2>
-        <p>qwefgfggggzx</p>
       </div>
     </div>
   )
